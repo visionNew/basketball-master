c9ba4d8720b627c1d40eaa099e85bff4e29c71f0
Vladislav Pavlov <vlad.z.pavlov@gmail.com>
2023-12-09 20:08:44 +0200

    add function handleFileUpload for reading fails

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  function handleFileUpload(e) {
+    e.preventDefault();
+    const file = e.target.files[0];
+    const reader = new FileReader();
 
+    reader.readAsText(file);
+    reader.onload = function () {
+      const dataArr = reader.result.split("\r\n");
+      const dataMatrix = dataArr
+        .filter((row) => row.trim().length !== 0)
+        .map((row) => row.split(","));
+      const sanitizedArray = dataMatrix.map((row) => {
+
+        return row.map((cell) => cell.trim());
+      });
+      return sanitizedArray;
+    };
+  }
 
   return (
     <>
       <div className="App">
-        <h1>Home Page</h1>
+        <h1>Basketball Statistic</h1>
+        <input type="file" onChange={handleFileUpload} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
